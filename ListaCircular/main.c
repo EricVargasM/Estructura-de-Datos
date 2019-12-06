2a3e84ae6d43d0f392d25361b586f05678ef8567
EricVargasM <56362007+EricVargasM@users.noreply.github.com>
2019-12-05 23:04:31 -0600

    Create main.c

---
+++ ListaCircular/main.c
@@ -0,0 +1,20 @@
+#include "listaligadacircular.h"
+
+int main(void) {
+
+  struct ListaLigada *milista = NULL;
+
+  insertaralprincipio(&milista, 10);
+  insertaralprincipio(&milista, 20);
+  insertaralprincipio(&milista, 30);
+  insertaralprincipio(&milista, 40);
+  insertaralprincipio(&milista, 50);
+  insertaralprincipio(&milista, 60);
+  insertaralprincipio(&milista, 70);
+
+  mostrar(milista);
+
+  printf("La lista contiene: %d elementos\n", numelementos(milista));
+
+  return 0;
+}
